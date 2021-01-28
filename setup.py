97c7e2ecb379b9bcbb45619cdbcfb34ef37902e9
IkbeomJeon <jeonikbeom@gmail.com>
2021-01-28 22:33:34 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="bop_toolkit", # Replace with your own username
+    version="1.0.0",
+    author="BOP Group",
+    author_email="bop.benchmark@gmail.com",
+    description="A Python toolkit of the BOP benchmark for 6D object pose estimation ",
+    url="https://github.com/thodan/bop_toolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+
+    ],
+    python_requires='>=3.6',
+)
